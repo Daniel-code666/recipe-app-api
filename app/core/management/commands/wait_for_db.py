56f32108ed02d79846576312fb2d8442a36e07fd
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2024-10-24 11:50:25 -0500

    correcciones lint

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 import time
 
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
